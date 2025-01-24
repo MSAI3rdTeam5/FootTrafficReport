8c6d916458171d5553fc1b5e48196ea48a9d10f7
Seungwoo-H1 <mwoo423@gmail.com>
2025-01-24 20:15:38 +0900

    perf: 성능 개선
    -botsort 모델을 사용하여 사람을 탐지하고 추적하는 기능을 개선
    -requirements.txt 라이브러리 수정
    
    Closes #13

--- people-detection/main.py
+++ people-detection/main.py
@@ -5,11 +5,12 @@ import torch
 import cv2
 
 class PersonTracker:
-    def __init__(self, model_path, result_dir='results/', tracker_config="bytetrack.yaml", conf=0.5, device=None,
+    def __init__(self, model_path, result_dir='results/', tracker_config="config/botsort.yaml", conf=0.5, device=None,
                  iou=0.5, img_size=(720, 1080)):
         # Set device to 'cuda:0' if GPU is available, else use 'cpu'
         self.device = device if device else ('cuda:0' if torch.cuda.is_available() else 'cpu')
         
+        # YOLO 모델 로드
         self.model = YOLO(model_path)
         self.result_dir = result_dir
         self.tracker_config = tracker_config
@@ -18,18 +19,20 @@ class PersonTracker:
         self.img_size = img_size
 
     def create_result_file(self):
-        folder_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        result_file_path = os.path.join(self.result_dir, folder_name + ".txt")
-        os.makedirs(self.result_dir, exist_ok=True)
+        folder_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")  # 현재 시간으로 폴더 이름 생성
+        result_file_path = os.path.join(self.result_dir, folder_name + ".txt")  # 결과 파일 경로
+        os.makedirs(self.result_dir, exist_ok=True)  # 결과 디렉토리가 없으면 생성
         with open(result_file_path, 'w') as file:
-            file.write(folder_name + "\n")
+            file.write(folder_name + "\n")  # 파일에 현재 폴더 이름을 기록
         return result_file_path
 
     def detect_and_track(self, source, show=True, logger=None):
-        result_file = self.create_result_file()
-        person_count = 0
-        previous_person_count = 0
+        # 객체 탐지 및 추적을 수행하는 함수
+        result_file = self.create_result_file()  # 결과 파일 생성
+        person_count = 0  # 현재 사람 수
+        previous_person_count = 0  # 이전 사람 수
 
+        # YOLO 모델을 사용하여 추적 시작
         results = self.model.track(
             source, show=show, stream=True, tracker=self.tracker_config, conf=self.conf,
             device=self.device, iou=self.iou, stream_buffer=True, classes=[0], imgsz=self.img_size
@@ -41,9 +44,11 @@ class PersonTracker:
                 id_count = boxes.id.int().tolist()
                 max_id = max(id_count)
 
+                # 새로운 사람이 탐지되었을 때 사람 수 갱신
                 if max_id > person_count:
                     person_count = max_id
 
+                # 사람이 추가될 때마다 결과 파일에 기록
                 if person_count != previous_person_count:
                     previous_person_count = person_count
                     with open(result_file, 'a') as filewrite:
@@ -71,7 +76,7 @@ class PersonTracker:
                     elif key == ord('q'):  # 'q'를 누르면 종료
                         print("Exiting...")
                         return
-
+                    
 ### Video
 if __name__ == '__main__':
     source = "data/street.webm"
