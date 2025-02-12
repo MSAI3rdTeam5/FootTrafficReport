8697d42e8e37084b8423f5096b27a4fbe4ef05c9
namgungcold <105970056+namgungcold@users.noreply.github.com>
2025-02-12 15:09:52 +0900

    docs: 주석 추가

--- report-generation/chan/visualization.py
+++ report-generation/chan/visualization.py
@@ -4,21 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# 전달 받은 데이터를 시각화 하는 과정
+
+# 원하는 기간의 데이터를 위해 데이터 기간 정리
 def filter_data_by_date(data, start_date, end_date):
     data['date'] = pd.to_datetime(data['date'])
     return data[(data['date'] >= start_date) & (data['date'] <= end_date)]
 
+# 각 image에 번호를 안붙히면 한번 만든 이미지로 계속 가기때문에 생성때마다 이미지에 고유 번호 붙이기
 def save_with_unique_name(folder, base_name, ext=".png"):
     if not os.path.exists(folder):
         os.makedirs(folder)
     timestamp = int(time.time())
     return os.path.join(folder, f"{base_name}_{timestamp}{ext}")
 
+# 그래프 font 설정
 def set_korean_font():
     plt.rcParams["font.family"] = "Malgun Gothic"
     plt.rcParams["axes.unicode_minus"] = False
 
-
+# 데이터 시각화 과정
 def create_visualizations(data, start_date=None, end_date=None):
     
     def set_korean_font():
@@ -28,7 +33,7 @@ def create_visualizations(data, start_date=None, end_date=None):
     set_korean_font()
     
     graph_paths = []
-    data_folder = "data_graph"
+    data_folder = "data_graph" #현재 로컬에선 data_graph경로로 설정해 활용중이지만 추후 경로수정 필요
 
     if start_date and end_date:
         data = filter_data_by_date(data, start_date, end_date)
@@ -40,8 +45,8 @@ def create_visualizations(data, start_date=None, end_date=None):
     # 요일별 유동인구
     weekday_order = ["월", "화", "수", "목", "금", "토", "일"]
     data['day_of_week'] = pd.Categorical(data['day_of_week'], categories=weekday_order, ordered=True)
-    plt.figure(figsize=(10, 6))
-    weekday_values = data.groupby("day_of_week")[age_columns].sum() / 1000  # 천 명 단위로 변환
+    plt.figure(figsize=(8, 5))
+    weekday_values = data.groupby("day_of_week")[age_columns].sum() / 1000  
     weekday_values_total = weekday_values.sum(axis=1)
     bars = weekday_values_total.plot(kind="bar", color="purple")
     plt.title("요일별 유동인구 (합계, 천 명)")
@@ -60,7 +65,7 @@ def create_visualizations(data, start_date=None, end_date=None):
     graph_paths.append(weekday_graph_path)
 
     # 연령별 유동인구
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(8, 5))
     age_values = data[age_columns].sum() / 1000  
     bars = age_values.plot(kind="bar", color=["skyblue", "pink", "blue", "lightcoral", "green", "lightgreen"])
     plt.title("연령별 유동인구 (합계, 천 명)")
@@ -86,7 +91,7 @@ def create_visualizations(data, start_date=None, end_date=None):
     labels = ["남성", "여성"]
     colors = ["skyblue", "pink"]
 
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(6, 6))
     wedges, texts, autotexts = plt.pie(
         gender_values, 
         labels=None,  
@@ -113,7 +118,7 @@ def create_visualizations(data, start_date=None, end_date=None):
     #시간대별 유동인구 그래프
     data['시간_숫자'] = pd.to_datetime(data['time'], format='%H:%M').dt.hour
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(8, 5))
     morning_values = data[data['시간_숫자'] < 12].groupby('시간_숫자')[age_columns].sum().sum(axis=1) / 1000  # 천 명 단위
     afternoon_values = data[data['시간_숫자'] >= 12].groupby('시간_숫자')[age_columns].sum().sum(axis=1) / 1000  # 천 명 단위
 
@@ -128,7 +133,7 @@ def create_visualizations(data, start_date=None, end_date=None):
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.legend()
 
-    
+    # 각 그래프를 이미지로 저장하고 그래프 경로를 return으로 전달    
     time_graph_path = save_with_unique_name(data_folder, "time_values_morning_afternoon_lines")
     plt.savefig(time_graph_path)
     plt.close()
