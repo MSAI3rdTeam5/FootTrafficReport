8666ea12e387ad6ebfe2e321d61c660471d73af0
Samdo3 <clcleh123@gmail.com>
2025-02-17 15:30:04 +0900

    feat : Celery(Worker & Beat)를 이용한 데이터 집계,데이터 정리
    - 추가된 파일:
    1. backend/celery_app.py: Celery 인스턴스(브로커 설정).
    2. backend/tasks.py: Celery Tasks & Periodic Task(Beat) 스케줄 설정.
    3. backend/app/cleanup.py: 오래된 데이터 정리 로직.
    4. backend/app/aggregate.py: 1시간 집계 로직.
    
    - 수정된 파일:
    1.docker-compose.yml: celery-worker, celery-beat, redis 서비스 추가.
    2. requirements.txt: Celery, redis 라이브러리 추가.
    
    feat #52

---
+++ backend/celery_app.py
@@ -0,0 +1,13 @@
+from celery import Celery
+import os
+
+
+BROKER_URL = os.environ.get("BROKER_URL")
+RESULT_BACKEND = None
+
+celery_app = Celery("foot_traffic", broker=BROKER_URL, backend=RESULT_BACKEND)
+
+celery_app.conf.update(
+    timezone="Asia/Seoul",
+    enable_utc=False,
+)
