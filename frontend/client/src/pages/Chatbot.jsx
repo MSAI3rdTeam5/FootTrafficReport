053c1a60e5553b9cbfcae78a2ca5dc40654d2b16
nakyung1007 <whskrud1007@naver.com>
2025-02-20 16:59:55 +0900

    refactor: api.js로 통합 #50

--- frontend/client/src/pages/Chatbot.jsx
+++ frontend/client/src/pages/Chatbot.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { getChatbotResponse } from "../services/chatbotService";
+import { getChatbotResponse } from "../utils/api";
 
 function ChatbotPage() {
   const location = useLocation();
