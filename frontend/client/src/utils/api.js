d1a99955bba572cbdc3034dc606ba5a366964f33
nakyung1007 <whskrud1007@naver.com>
2025-02-21 18:25:12 +0900

    fix: 챗봇 url 수정  #50

--- frontend/client/src/utils/api.js
+++ frontend/client/src/utils/api.js
@@ -50,42 +50,10 @@ export async function callReportGeneration(title, dataSummary) {
     throw error;
   }
 }
-// azure container를 껴서 넣는 형식... DB가 필요 거쳐야 할수도.. 토큰 생성 후 받아야 함!!
 
-// import { io } from "socket.io-client"
-// const socket io("https://msteam5iseeu.ddns.net");
-
-//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-//chatbot API 호출 예시
-// export async function getChatbotResponse(userQuestion) {
-//   try {
-//     const response = await fetch("/chatbot/ask", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ question: userQuestion }),
-//     });
-
-//     const data = await response.json(); // 응답 JSON 파싱
-
-//     // 챗봇이 정보를 찾지 못한 경우 처리
-//     if (
-//       data ===
-//       "제가 해당 질문에 대한 정보를 찾지 못했습니다. 다른 질문을 시도해 주세요!"
-//     ) {
-//       return "죄송합니다. 관련 정보를 찾을 수 없습니다. 다른 질문을 해주세요!";
-//     }
-
-//     return data; // 정상적인 챗봇 응답 반환
-//   } catch (error) {
-//     console.error("Chatbot API 호출 중 오류 발생:", error);
-//     return "오류가 발생했습니다. 잠시 후 다시 시도해 주세요.";
-//   }
-// }
 export async function getChatbotResponse(userQuestion) {
   try {
-    const endpoint = "https://msteam5iseeu.ddns.net/chatbot/ask"; // 직접 DDNS 주소 사용
+    const endpoint = `https://msteam5iseeu.ddns.net/chatbot/ask?question=${userQuestion}`;
 
     const payload = { question: userQuestion };
 
