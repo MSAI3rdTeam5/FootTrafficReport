9903ea14cb8e9c4ae13be291984b0838b24e8fe7
nakyung1007 <whskrud1007@naver.com>
2025-02-21 10:16:07 +0900

    chore: Vite proxy 설정 추가

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -5,5 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true, // 개발 서버 구동 시 자동으로 브라우저 열기 (선택)
+    proxy: {
+      "/api": {
+        target: "https://msteam5iseeu.ddns.net",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
